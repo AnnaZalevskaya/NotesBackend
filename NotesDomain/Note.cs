7828369a2d240c3a651163f2c93fa7dd6bfafe8d
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-10-02 00:11:35 +0300

    rename-attribute

--- NotesDomain/Note.cs
+++ NotesDomain/Note.cs
@@ -4,7 +4,7 @@ namespace NotesDomain
 {
     public class Note
     {
-        public Guid UserUd { get; set; }
+        public Guid UserId { get; set; }
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Details { get; set; }
