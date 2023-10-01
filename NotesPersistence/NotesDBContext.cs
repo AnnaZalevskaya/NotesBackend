df204538bd38f7c900aadbccf2146127fbdb8fb1
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-10-01 23:14:54 +0300

    add NotesDBContext

---
+++ NotesPersistence/NotesDBContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesApplication.Interfaces;
+using NotesDomain;
+using NotesPersistence.EntityTypeConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesPersistence
+{
+    public class NotesDBContext : DbContext, INoteDBContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        public NotesDBContext(DbContextOptions<NotesDBContext> options) 
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
