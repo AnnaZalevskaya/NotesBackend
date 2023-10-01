2a62e781414d659fc45e884cd80371dccb460426
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-10-01 23:01:33 +0300

    add NoteConfig

---
+++ NotesPersistence/EntityTypeConfigurations/NoteConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesDomain;
+
+namespace NotesPersistence.EntityTypeConfigurations
+{
+    public class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            builder.HasKey(note => note.Id);
+            builder.HasIndex(note => note.Id);
+            builder.Property(note => note.Title).HasMaxLength(250);
+        }
+    }
+}
