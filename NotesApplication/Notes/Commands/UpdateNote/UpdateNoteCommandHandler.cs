81bdc622bb211c98e410c0263d61c91b5be64072
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-10-02 00:22:08 +0300

    setting-update-note

---
+++ NotesApplication/Notes/Commands/UpdateNote/UpdateNoteCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NotesApplication.Interfaces;
+
+namespace NotesApplication.Notes.Commands.UpdateNote
+{
+    public class UpdateNoteCommandHandler : IRequest<UpdateNoteCommand>
+    {
+        private readonly INoteDBContext _dbContext;
+
+        public UpdateNoteCommandHandler(INoteDBContext dbContext) => 
+            _dbContext = dbContext;
+
+        public async Task<Unit> Handle(UpdateNoteCommand request, 
+            CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Notes
+                .FirstOrDefaultAsync(n => n.Id == request.Id, cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+
+            }
+
+            return Unit.Value;
+        }
+    }
+}
