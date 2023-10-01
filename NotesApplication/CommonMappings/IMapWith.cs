a973627670bfa6cc40bc703f99184898342cf085
AnnaZalevskaya <123122231+AnnaZalevskaya@users.noreply.github.com>
2023-10-01 23:49:46 +0300

    using automapper create mapconfig (#2)

---
+++ NotesApplication/CommonMappings/IMapWith.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+
+namespace NotesApplication.CommonMappings
+{
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) => 
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
