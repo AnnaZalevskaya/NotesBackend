4d9555e128011949e75dda12c58058cde6080bae
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-10-01 23:07:07 +0300

    add DBContext

---
+++ NotesApplication/Interfaces/INoteDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApplication.Interfaces
+{
+    public interface INoteDBContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangesAsync (CancellationToken cancellationToken);
+    }
+}
