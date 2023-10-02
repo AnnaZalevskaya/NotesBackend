d37f723b8739ffe9cd478086135ec17c89d7c58d
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-10-02 19:25:41 +0300

    add-query-of-notes

---
+++ NotesApplication/Notes/Queries/GetNoteList/GetNoteListQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NotesApplication.Interfaces;
+
+namespace NotesApplication.Notes.Queries.GetNoteList
+{
+    public class GetNoteListQueryHandler 
+        : IRequestHandler<GetNoteListQuery, NoteListVm>
+    {
+        private readonly INoteDBContext _dBContext;
+        private readonly IMapper _mapper;
+
+        public GetNoteListQueryHandler(INoteDBContext dBContext, IMapper mapper)
+        {
+            _dBContext = dBContext;
+            _mapper = mapper;
+        }
+
+        public async Task<NoteListVm> Handle(GetNoteListQuery request, CancellationToken cancellationToken)
+        {
+            var notesQuery = await _dBContext.Notes
+                .Where(note => note.UserId == request.UserId)
+                .ProjectTo<NoteLookupDTO>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            return new NoteListVm { Notes = notesQuery };
+        }
+    }
+}
---
+++ NotesApplication/Notes/Queries/GetNoteList/NoteLookupDTO.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using NotesApplication.CommonMappings;
+using NotesDomain;
+
+namespace NotesApplication.Notes.Queries.GetNoteList
+{
+    public class NoteLookupDTO : IMapWith<Note>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Note, NoteLookupDTO>()
+                .ForMember(noteDTO => noteDTO.Id, 
+                opt => opt.MapFrom(note => note.Id))
+                .ForMember(noteDTO => noteDTO.Title,
+                opt => opt.MapFrom(note => note.Title));
+        }
+    }
+}
