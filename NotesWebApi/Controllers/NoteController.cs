7820da59ad0b2f98c7ade91cf823ae8a75cc1aa6
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-10-03 20:17:57 +0300

    add-notecontroller

---
+++ NotesWebApi/Controllers/NoteController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotesApplication.Notes.Queries.GetNoteDetails;
+using NotesApplication.Notes.Queries.GetNoteList;
+
+namespace NotesWebApi.Controllers
+{
+    public class NoteController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<NoteListVm>> GetAll()
+        {
+            var query = new GetNoteListQuery
+            {
+                UserId = UserId
+            };
+            var vm = await Mediator.Send(query);
+
+            return Ok(vm);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<NoteDetailsVm>> Get(Guid id)
+        {
+            var query = new GetNoteDetailsQuery
+            {
+                UserId = UserId,
+                Id = id
+            };
+            var vm = await Mediator.Send(query);
+
+            return Ok(vm);
+        }
+    }
+}
