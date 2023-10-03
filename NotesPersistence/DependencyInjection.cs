170536272d5cdf1861030acf52b36bbcf661337a
AnnaZalevskaya <123122231+AnnaZalevskaya@users.noreply.github.com>
2023-10-03 17:43:24 +0300

    Setting di (#4)
    * add-class-DI
    
    * set-DI-in-app

---
+++ NotesPersistence/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NotesApplication.Interfaces;
+
+namespace NotesPersistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            var connectionString = configuration["DBConnection"];
+            services.AddDbContext<NotesDBContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<INoteDBContext>(provider =>
+            provider.GetService<NotesDBContext>());
+
+            return services;
+        }   
+    }
+}
