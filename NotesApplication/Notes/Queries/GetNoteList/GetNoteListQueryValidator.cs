e37f491349d9a735d9939ff9bbf9a178afd365e0
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-10-06 22:56:37 +0300

    add-validators-for-queries

---
+++ NotesApplication/Notes/Queries/GetNoteList/GetNoteListQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace NotesApplication.Notes.Queries.GetNoteList
+{
+    public class GetNoteListQueryValidator :AbstractValidator<GetNoteListQuery>
+    {
+        public GetNoteListQueryValidator()
+        {
+            RuleFor(note => note.UserId)
+                .NotEqual(Guid.Empty);
+        }
+    }
+}
