94ee3d7acfeb99e5f2c3a3d2d92d62e820a40754
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-10-07 16:09:00 +0300

    add-extentionsmiddleware

---
+++ NotesWebApi/Middleware/CustomExceptionHandlerMiddewareExtentions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace NotesWebApi.Middleware
+{
+    public static class CustomExceptionHandlerMiddewareExtentions
+    {
+        public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomExceptionHandlerMiddeware>();
+        }
+    }
+}
