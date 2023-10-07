e374192feef52494b9cd485bfed1d51cabd7607f
AnnaZalevskaya <123122231+AnnaZalevskaya@users.noreply.github.com>
2023-10-07 15:48:50 +0300

    Add validation (#6)
    * add-validators-for-commands
    
    * add-validators-for-queries
    
    * using-validation

---
+++ NotesApplication/Common/Behaviors/ValidationBehavvior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using FluentValidation;
+
+namespace NotesApplication.Common.Behaviors
+{
+    public class ValidationBehavvior<TRequest, TResponse> 
+        : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<IRequest>> _validators;
+
+        public ValidationBehavvior(IEnumerable<IValidator<IRequest>> validators)
+        {
+            _validators = validators;   
+        }
+
+        public Task<TResponse> Handle (TRequest request, RequestHandlerDelegate<TResponse> next, 
+            CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var failures = _validators
+                .Select(v => v.Validate(context))
+                .SelectMany(res => res.Errors)
+                .Where(failure => failure != null)
+                .ToList();
+            if(failures.Count() != 0)
+            {
+                throw new ValidationException(failures);
+            }
+
+            return next();
+        }
+    }
+}
---
+++ NotesApplication/Notes/Commands/DeleteNote/DeleteNoteCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace NotesApplication.Notes.Commands.DeleteNote
+{
+    public class DeleteNoteCommandValidator : AbstractValidator<DeleteNoteCommand>
+    {
+        public DeleteNoteCommandValidator()
+        {
+            RuleFor(note => note.Id)
+                .NotEqual(Guid.Empty);
+            RuleFor(note => note.UserId)
+                .NotEqual(Guid.Empty);
+        }
+    }
+}
