b2cd74b379742f9cee7fb157af665730a8c38609
AnnaZalevskaya <123122231+AnnaZalevskaya@users.noreply.github.com>
2023-10-16 11:15:18 +0300

    Create middleware (#7)
    * add-customhandlermiddleware
    
    * add-extentionsmiddleware
    
    * use-middleware

---
+++ NotesWebApi/Middleware/CustomExceptionHandlerMiddeware.cs
@@ -0,0 +1,55 @@
+﻿using FluentValidation;
+using NotesApplication.Common.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace NotesWebApi.Middleware
+{
+    public class CustomExceptionHandlerMiddeware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomExceptionHandlerMiddeware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                await HandeExceptionAsync(context, exception);
+            }
+        }
+
+        private Task HandeExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var result = string.Empty;
+            
+            switch (exception)
+            {
+                case ValidationException validationException:
+                    code = HttpStatusCode.BadRequest;
+                    result = JsonSerializer.Serialize(validationException.Errors);
+                    break;
+                case NotFoundException:
+                    code = HttpStatusCode.NotFound;
+                    break;
+            }
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+
+            if (result == string.Empty)
+            {
+                result = JsonSerializer.Serialize(new { error = exception.Message });
+            }
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
